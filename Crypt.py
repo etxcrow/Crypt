050024a5343f71d9ff09817d79d7c757866e8a72
Min Min <62245372+etxcrow@users.noreply.github.com>
2020-03-29 04:37:22 -0400

    Add files via upload

---
+++ Crypt.py
@@ -0,0 +1,48 @@
+print ("=================================")
+print ("Basic Level Testing,Idiot Level  ")
+print ("Try To Get Password              ")
+print ("=================================")
+print ("=================================")
+print ("Hint : if u don't know how to do ")
+print ("You need to learn basic python skill      ")
+print ("=================================")
+user_input = input("Enter Password : ")
+if user_input == "thekey002":
+ print("          ========               ")
+ print("          Congrats               ")
+ print("          ========               ")
+ print("  ______________________________ ")
+ print(" |     ~~                       |")
+ print(" |    {(0)>    EVIL_CROW        |")
+ print(" |    /(|)\       _________     |")
+ print(" |   /_(|)_\	 |FREE USER|    |")
+ print(" |     /_\	 -----------    |")
+ print(" |______________________________|")
+ print("      dev-minmin      secret-msg ")
+
+ print("====================================")
+ print("Choose your prefer tool input number")
+ print("====================================")
+ print("                                    ")
+ print("1. Simple Encrypt words                    ")
+ print("2. Simple Decrypt words                    ")
+ print("0. Quit")
+ while True:
+ 
+  user_input = input("crow: > ")
+  if user_input == "0":
+    print("Exiting....")
+    print("Exited")
+    break
+  elif user_input == "1":
+    key  = (input("Words at most 20 character: "))
+
+    result = ("u8tpipr"+"********lastmanf"+"thenlet"+"th****ia         bghhthismysnmtthroktiyjtyoutheyouthisthryjunubnoobthe"+key+"                    maniosg"+"thekjdhjglastlfdmenjusticeng   fhofuvkkerleeloplkwetthisis ntosajokeiamoutdtandingtobe        aprototyperogf   dfghfforthei,mptirotyhrgethtiddsdisyhisshitfog     myanmar        anfthniwillletyouyunmyogremoreontopifcanfthreleyr   forthe************************decryptrfdwoprds")    
+    print("Crypted to: "+result)
+  elif user_input == "2":
+    code = (input("Input Encoded words:- ")) 
+    print ("Decoded to: "+code[100:120:])    
+
+
+
+
